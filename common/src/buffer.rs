31072bff966235c4e49354b9552d61e85d7f81e9
Zach Madsen <zach@zachmadsen.com>
2023-12-03 21:49:42 -0600

    Use better variable names in TripleBuffer

--- common/src/buffer.rs
+++ common/src/buffer.rs
@@ -6,68 +6,91 @@ use std::{
     },
 };
 
-const INIT_BACK_IDX: u8 = 0;
-const INIT_WRITER_IDX: u8 = 1;
-const INIT_READER_IDX: u8 = 2;
-
-const UPDATE_FLAG: u8 = 0b00000100;
 const INDEX_MASK: u8 = 0b00000011;
+const UPDATE_FLAG_MASK: u8 = 0b00000100;
 
+// TODO: Add unit tests (with loom?).
 struct TripleBuffer<T> {
-    bufs: [UnsafeCell<T>; 3],
-    back_idx: AtomicU8,
+    buffers: [UnsafeCell<T>; 3],
+    back_buffer_index: AtomicU8,
 }
 
-pub fn triple_buffer<T: Clone>(val: T) -> (Writer<T>, Reader<T>) {
-    let buf = Arc::new(TripleBuffer {
-        bufs: [
-            UnsafeCell::new(val.clone()),
-            UnsafeCell::new(val.clone()),
-            UnsafeCell::new(val),
+pub fn triple_buffer<T: Clone>(
+    buffer: T,
+) -> (TripleBufferWriter<T>, TripleBufferReader<T>) {
+    const INIT_BACK_BUFFER_INDEX: u8 = 0;
+    const INIT_WRITE_BUFFER_INDEX: u8 = 1;
+    const INIT_READ_BUFFER_INDEX: u8 = 2;
+
+    let triple_buffer = Arc::new(TripleBuffer {
+        buffers: [
+            UnsafeCell::new(buffer.clone()),
+            UnsafeCell::new(buffer.clone()),
+            UnsafeCell::new(buffer),
         ],
-        back_idx: AtomicU8::new(INIT_BACK_IDX),
+        back_buffer_index: AtomicU8::new(INIT_BACK_BUFFER_INDEX),
     });
-    let writer = Writer { buf: buf.clone(), idx: INIT_WRITER_IDX };
-    let reader = Reader { buf, idx: Cell::new(INIT_READER_IDX) };
+    let writer = TripleBufferWriter {
+        triple_buffer: triple_buffer.clone(),
+        index: INIT_WRITE_BUFFER_INDEX,
+    };
+    let reader = TripleBufferReader {
+        triple_buffer,
+        index: Cell::new(INIT_READ_BUFFER_INDEX),
+    };
+
     (writer, reader)
 }
 
-pub struct Writer<T> {
-    buf: Arc<TripleBuffer<T>>,
-    idx: u8,
+pub struct TripleBufferWriter<T> {
+    triple_buffer: Arc<TripleBuffer<T>>,
+    index: u8,
 }
 
-unsafe impl<T: Send> Send for Writer<T> {}
+unsafe impl<T: Send> Send for TripleBufferWriter<T> {}
 
-impl<T> Writer<T> {
+impl<T> TripleBufferWriter<T> {
     pub fn get_mut(&mut self) -> &mut T {
-        unsafe { &mut *self.buf.bufs[self.idx as usize].get() }
+        unsafe { &mut *self.triple_buffer.buffers[self.index as usize].get() }
     }
 
     pub fn swap(&mut self) {
-        let new_back_index = self.idx | UPDATE_FLAG;
-        let old_back_index =
-            self.buf.back_idx.swap(new_back_index, Ordering::SeqCst);
-        self.idx = old_back_index & INDEX_MASK;
+        let new_back_index = self.index | UPDATE_FLAG_MASK;
+        // TODO: Figure out better memory orderings for the swaps. They're all
+        // SeqCst because it's probably correct, but I'm leaving performance on
+        // the table.
+        let old_back_index = self
+            .triple_buffer
+            .back_buffer_index
+            .swap(new_back_index, Ordering::SeqCst);
+        self.index = old_back_index & INDEX_MASK;
     }
 }
 
-pub struct Reader<T> {
-    buf: Arc<TripleBuffer<T>>,
-    idx: Cell<u8>,
+pub struct TripleBufferReader<T> {
+    triple_buffer: Arc<TripleBuffer<T>>,
+    index: Cell<u8>,
 }
 
-unsafe impl<T: Send> Send for Reader<T> {}
+unsafe impl<T: Send> Send for TripleBufferReader<T> {}
 
-impl<T> Reader<T> {
+impl<T> TripleBufferReader<T> {
     pub fn get(&self) -> &T {
-        // Update the reader index if the writer updated the back buffer.
-        if self.buf.back_idx.load(Ordering::SeqCst) & UPDATE_FLAG != 0 {
-            let new_back_index = self.idx.get();
-            let old_back_index =
-                self.buf.back_idx.swap(new_back_index, Ordering::SeqCst);
-            self.idx.set(old_back_index & INDEX_MASK);
+        // Swap the read and back buffers if there's an update.
+        if self.triple_buffer.back_buffer_index.load(Ordering::SeqCst)
+            & UPDATE_FLAG_MASK
+            != 0
+        {
+            let new_back_index = self.index.get();
+            let old_back_index = self
+                .triple_buffer
+                .back_buffer_index
+                .swap(new_back_index, Ordering::SeqCst);
+            self.index.set(old_back_index & INDEX_MASK);
         }
-        unsafe { &*self.buf.bufs[self.idx.get() as usize].get() }
+
+        unsafe {
+            &*self.triple_buffer.buffers[self.index.get() as usize].get()
+        }
     }
 }
