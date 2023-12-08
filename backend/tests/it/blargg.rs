4047fe768c53543a3c8f344975512f05355d5b76
Zach Madsen <zach@zachmadsen.com>
2023-12-08 15:32:39 -0600

    Refactor integration tests to make adding new tests easier

---
+++ backend/tests/it/blargg.rs
@@ -0,0 +1,50 @@
+use std::fs;
+
+use backend::{ppu, Emu};
+use common::TripleBuffer;
+
+const STATUS_ADDR: u16 = 0x6000;
+const OUTPUT_ADDR: u16 = 0x6004;
+const RUNNING_STATUS: u8 = 0x80;
+
+fn run(dir: &str) {
+    for entry in fs::read_dir(dir).unwrap() {
+        let entry = entry.unwrap();
+
+        let rom = fs::read(entry.path()).unwrap();
+        let buffer: Box<[u8; ppu::BUFFER_SIZE]> =
+            vec![0u8; ppu::BUFFER_SIZE].try_into().unwrap();
+        let (writer, _) = TripleBuffer::new(buffer);
+        // TODO: Reuse the same emulator instance across the ROMs to test
+        // resets and loading new ROMs.
+        let mut emu = Emu::new(&rom, writer);
+
+        let mut status = emu.read(STATUS_ADDR);
+        while status != RUNNING_STATUS {
+            emu.step();
+            status = emu.read(STATUS_ADDR);
+        }
+
+        while status == RUNNING_STATUS {
+            emu.step();
+            status = emu.read(STATUS_ADDR);
+        }
+
+        let mut output = Vec::new();
+        let mut addr = OUTPUT_ADDR;
+        let mut byte = emu.read(addr);
+        while byte != b'\0' {
+            output.push(byte);
+            addr += 1;
+            byte = emu.read(addr);
+        }
+
+        let output = String::from_utf8_lossy(&output);
+        assert!(output.contains("Passed"), "{}", output);
+    }
+}
+
+#[test]
+fn instr() {
+    run("../roms/instr_test-v5/")
+}
--- backend/tests/it/main.rs
+++ backend/tests/it/main.rs
@@ -1,1 +1,1 @@
-mod instr;
+mod blargg;
